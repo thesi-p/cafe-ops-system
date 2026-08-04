2cfd989b58401ffe856830fe5d1786f12b1c6d77
thesi-p <thesihan.p@gmail.com>
2026-08-04 11:30:28 +0530

    Style change it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { AppProvider } from './context/AppContext';
 import Dashboard      from './Pages/Dashboard';
 import ToastContainer from './components/Toast';
+import DemoModal      from './components/DemoModal';
 import './index.css';
 
 function App() {
   return (
     <AppProvider>
+      <DemoModal />
       <Dashboard />
       <ToastContainer />
     </AppProvider>
